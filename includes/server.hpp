87e44c8b616b901e982c2cb1b21eaa070814a9cf
PrashanthaTP <tpprashantha@gmail.com>
2024-12-08 16:46:21 +0000

    Feat: Start using epoll method (in progress)
    * Idea:
      -> HttpServer ->  CreateSocket()
      -> Start() -> SetSockOpt() -> Bind() -> Listen()
      -> SetupEpoll() -> Create Epoll Fds
      -> SetupThreads() -> Setup listener and worker threads
      -> ListenerThread -> AcceptConnections -> Epoll Add
      -> WorkerThreads -> HandleConnections -> Epoll Wait

--- includes/server.hpp
+++ includes/server.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <stdint.h>
 #include <string>
+#include <thread>
 
 namespace SimpleHttpServer {
+
+const uint16_t g_thread_poolsize = 5;
+const uint16_t g_max_backlog = 3;
+
 class HttpServer {
    public:
     //TODO: delete certain constructors
@@ -10,14 +16,30 @@ class HttpServer {
     HttpServer(const std::string& t_port_str);
     ~HttpServer();
     void start();
+    void stop();
 
    private:
     int m_server_fd;
     const std::string m_port_str;
     struct addrinfo* m_server_addrinfo_p;
+
     bool m_is_running = false;
+    bool m_is_stopped = false;
+
+    int m_epoll_fds[g_thread_poolsize];
+    std::thread m_listener_thread;
+    std::thread m_worker_threads[g_thread_poolsize];
+
     void createSocket();
+    void closeSocket();
+
     void acceptConnections();
-    void handleClient(int client_fd);
+    void handleConnections();
+
+    void setupEpoll();
+    void closeEpoll();
+
+    void setupThreads();
+    void joinThreads();
 };
 }  // namespace SimpleHttpServer
--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/epoll.h>
 #include <unistd.h>  //close
 #include <cstring>   //memset
 #include <iostream>  //cout
@@ -12,34 +13,15 @@
 using std::cout;
 
 namespace SimpleHttpServer {
+//TODO: replace cout and exist_with_msg with something different
 HttpServer::HttpServer(const std::string& t_port_str) : m_port_str(t_port_str) {
     createSocket();
 }
 
 HttpServer::~HttpServer() {
-    if (m_server_fd < 0) {
-        close(m_server_fd);
+    if (!m_is_stopped) {
+        stop();
     }
-}
-
-void HttpServer::start() {
-    int sockopt = 1;
-    setsockopt(m_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &sockopt,
-               sizeof(sockopt));
-
-    if (bind(m_server_fd, m_server_addrinfo_p->ai_addr,
-             m_server_addrinfo_p->ai_addrlen) < 0) {
-        exit_with_msg("Error during binding socket");
-    }
-    //print_m_server_addrinfo_p(m_server_addrinfo_p);
-
-    freeaddrinfo(m_server_addrinfo_p);
-    if (listen(m_server_fd, 10) < 0) {
-        exit_with_msg("Error during listening");
-    } else {
-        cout << "Server started listening..\n";
-    }
-    acceptConnections();
 }
 
 void HttpServer::createSocket() {
@@ -63,68 +45,55 @@ void HttpServer::createSocket() {
     }
 }
 
-// int countThread = 0;
-void HttpServer::acceptConnections() {
-    //client addr storage
-    struct sockaddr_storage client_addr;
-    socklen_t client_addr_size = sizeof(client_addr);
-    //accept connections
-    //multithreading -> handle clients
-    while (1) {
-        int client_fd;
-        client_fd = accept(m_server_fd, (struct sockaddr*)&client_addr,
-                           &client_addr_size);
-        if (client_fd < 1) {
-            log_err("Error while accepting client connection");
-            continue;
-        }
-        std::thread client_thread(&HttpServer::handleClient, this, client_fd);
-        client_thread.detach();
+void HttpServer::start() {
+    int sockopt = 1;
+    setsockopt(m_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &sockopt,
+               sizeof(sockopt));
+
+    if (bind(m_server_fd, m_server_addrinfo_p->ai_addr,
+             m_server_addrinfo_p->ai_addrlen) < 0) {
+        exit_with_msg("Error during binding socket");
     }
-}
-int checkReceiveError(int bytes_received) {
-    if (bytes_received < 0) {
-        //?
-        log_msg("Error while receiving Client msg");
-        return -1;
-    } else if (bytes_received == 0) {
-        log_msg("Client has closed the connection");
-        return -1;
+    //print_m_server_addrinfo_p(m_server_addrinfo_p);
+    freeaddrinfo(m_server_addrinfo_p);  //should this be moved to destructor
+
+    if (listen(m_server_fd, g_max_backlog) < 0) {
+        exit_with_msg("Error during listening");
     } else {
+        cout << "Server started listening..\n";
     }
-    return 0;
+    // acceptConnections();
+
+    setupEpoll();
+    setupThreads();
 }
 
-void HttpServer::handleClient(int client_fd) {
-    // countThread++;
-    // cout << "Thread: " << countThread << "\n";
-    Request request;
-    int bytes_received =
-        recv(client_fd, request.m_buffer, sizeof(request.m_buffer), 0);
-    // if (bytesReceived <= 0) {
-    if (checkReceiveError(bytes_received) < 0) {
-        close(client_fd);
-        return;
+void HttpServer::setupEpoll() {
+    for (int i = 0; i < g_thread_poolsize; i++) {
+        m_epoll_fds[i] = epoll_create1(0);
+        if (m_epoll_fds[i] < 0) {
+            exit_with_msg("Error creating epoll fd");
+        }
     }
-    cout << "Incoming request : \n";
-    cout << request.m_buffer << "\n";
-    cout << "--------------------\n";
-    request.parse();
-    Response response;
-    response.setStatusCode(HttpStatusCode::Ok);
-    response.setHeader("Content-Type", "text/html");
-    response.setContent("<h1>Hello World</h1>\r\n");
-    // response.setContent("<h1>Hello World " + std::to_string(countThread) + "</h1>\r\n");
-    response.parse();
-    //what if `send` doesn't send the whole buffer?
-    //what to be done on partial transfer?
-    //anyway for current use case it almost always sends the whole message
-    //considering the message size
-    send(client_fd, response.str().c_str(), response.size(), 0);
-    cout << response.str() << "\n";
-    cout << "Response sent\n";
-    cout << "--------------------\n";
-    //close(client_fd);
+}
+
+void HttpServer::setupThreads() {
+    m_listener_thread = std::thread(&HttpServer::acceptConnections, this);
+    for (int i = 0; i < g_thread_poolsize; i++) {
+        m_worker_threads[i] =
+            std::thread(&HttpServer::handleConnections, this, i);
+    }
+}
+
+void HttpServer::acceptConnections() {}
+
+void HttpServer::handleConnections() {}
+
+void HttpServer::stop() {
+    if (m_server_fd < 0) {
+        close(m_server_fd);
+    }
+    m_is_stopped = true;
 }
 
 }  // namespace SimpleHttpServer
